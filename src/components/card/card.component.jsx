e84f1c9b6c07fb25efaef39268c17ae61c0b194a
Luis Villegas <luchoavak@gmail.com>
2023-01-17 14:05:43 -0600

    end of all funtional components

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,17 +1,13 @@
-import { Component } from "react";
 import './card.styles.css'
 
-class Card extends Component{
-    render(){
-        const {monster}=this.props;
-        const { name, email, id } = monster;
+const Card = ({ monster }) =>{
+        const { id, name, email } = monster;
         return(
             <div className="card-container" key={id}>
-                <img alt={`monster ${name}`} src={`https://robohash.org/${monster.id}?set=set2`} />
+                <img alt={`monster ${name}`} src={`https://robohash.org/${id}?set=set2`} />
                 <h2>{name}</h2>
                 <p>{email}</p>
-            </div>)
-    }
+            </div>);
 }
 
 export default Card;
